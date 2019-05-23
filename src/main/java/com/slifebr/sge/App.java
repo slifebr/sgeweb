3bd7df6024e11b59094ac1227c0e4c0981cd6d05
TanderaSys <38049790+TanderaSys@users.noreply.github.com>
2019-05-23 16:34:17 -0300

    inicio - configuração do pom

---
+++ src/main/java/com/slifebr/sge/App.java
@@ -0,0 +1,13 @@
+package com.slifebr.sge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
